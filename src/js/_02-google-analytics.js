b8290491b49fa30ccf16af474d6bd49de1c88a26
Yexan <contact.adrien.martinet@gmail.com>
2015-10-20 15:43:14 +0200

    Add analytics and twitter widget

--- src/js/_02-google-analytics.js
+++ src/js/_02-google-analytics.js
@@ -2,10 +2,10 @@
  * Put your Google Analytics ID and uncomment
  */
 
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-//     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-//     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-// ga('create', 'YOUR-UA-ID', 'auto');
-// ga('send', 'pageview');
+ga('create', 'UA-27913447-1', 'auto');
+ga('send', 'pageview');
